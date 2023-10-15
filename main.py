04a807ed4f21f5b76854abfb9f2b5d27a49a3277
VikaRayskaya <rayskaya.viktorya@gmail.com>
2023-10-15 20:41:08 +0300

    add homework ex1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+a = int(input())
+b = int(input())
+c = int(input())
+
+e = a + b + c
+d = e / 3
+print("среднее", d)
+
+g = 1 / a + 1 / b + 1 / c
+f = 3 / g
+print("гармоническое", f)
+
+data = sorted([ a, b, c])
+print("медиана", data[1])
+
+data = sorted([ a, b, c])
+y = data[2] ** data[0] / d
+print("абадакадабра", y)
